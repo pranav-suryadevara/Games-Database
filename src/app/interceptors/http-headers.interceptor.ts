fd1068ca453acece2f30774fa42bfd74c40bc290
Pranav <pranav.suryadevara11@gmail.com>
2023-01-24 12:10:40 +0530

    Angular Game Project

--- src/app/interceptors/http-headers.interceptor.ts
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -1,6 +1,11 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class HttpHeadersInterceptor implements HttpInterceptor {
@@ -12,12 +17,12 @@ export class HttpHeadersInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     req = req.clone({
       setHeaders: {
-        'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+        'x-rapidapi-key': 'de52f4499cmsh8b4eebb9eeaaf45p10cc88jsnfa4bba38169f',
         'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
       },
       setParams: {
         key: 'e40e743af2c94b0c916a8aa618fb4473',
-      }
+      },
     });
     return next.handle(req);
   }
